f7e0d3d000631117a823c65e4dcedbd93718cedf
Benja Cornejo <benjalessandro@outlook.com>
2015-03-26 09:20:06 -0300

    Creación de main

---
+++ EcuacionCuadratica.java
@@ -0,0 +1,17 @@
+package ecuacionCuadratica;
+
+/*
+ * @author Benja
+ */
+ 
+public class EcuacionCuadratica {
+
+    public static void main(String[] args) {
+
+        Interfaz i = new Interfaz();
+        i.setResizable(false);
+        i.setLocationRelativeTo(null);
+        i.pack();
+        i.setVisible(true);
+    }
+}
